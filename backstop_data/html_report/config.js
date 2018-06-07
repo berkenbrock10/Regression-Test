0a4b0cd8ad7e400694395f1076e88c957e4c3728
Christian <christian.berkenbrock@chaordicsystems.com>
2018-06-07 11:26:36 -0300

    first commit

---
+++ backstop_data/html_report/config.js
@@ -0,0 +1,22 @@
+report({
+  "testSuite": "BackstopJS",
+  "tests": [
+    {
+      "pair": {
+        "reference": "../bitmaps_reference/test_onsite_Polishop_HOME_0_chaordic_0_desktop.png",
+        "test": "../bitmaps_test/20180607-110123/test_onsite_Polishop_HOME_0_chaordic_0_desktop.png",
+        "selector": "#chaordic",
+        "fileName": "test_onsite_Polishop_HOME_0_chaordic_0_desktop.png",
+        "label": "Polishop_HOME",
+        "requireSameDimensions": true,
+        "misMatchThreshold": 0.1,
+        "url": "http://www.polishop.com.br/",
+        "referenceUrl": "",
+        "engineErrorMsg": "Navigation Timeout Exceeded: 60000ms exceeded",
+        "error": "Reference file not found /home/christianberkenbrock/Documents/test-backstop-js/backstop_data/bitmaps_reference/test_onsite_Polishop_HOME_0_chaordic_0_desktop.png"
+      },
+      "status": "fail"
+    }
+  ],
+  "id": "test_onsite"
+});
